ce42aa6c1dd464b4410cbfcffd2670dc8ce13827
benjiJG <benjii.goodman@hotmail.com>
2015-01-05 19:44:18 +0000

    Second Commit

--- GyabzSS/Form1.Designer.cs
+++ GyabzSS/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnScreenCap = new System.Windows.Forms.Button();
+            this.btnAreaCap = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnScreenCap
+            // 
+            this.btnScreenCap.Location = new System.Drawing.Point(12, 12);
+            this.btnScreenCap.Name = "btnScreenCap";
+            this.btnScreenCap.Size = new System.Drawing.Size(210, 48);
+            this.btnScreenCap.TabIndex = 0;
+            this.btnScreenCap.Text = "Capture Screen";
+            this.btnScreenCap.UseVisualStyleBackColor = true;
+            this.btnScreenCap.Click += new System.EventHandler(this.btnScreenCap_Click);
+            // 
+            // btnAreaCap
+            // 
+            this.btnAreaCap.Enabled = false;
+            this.btnAreaCap.Location = new System.Drawing.Point(12, 66);
+            this.btnAreaCap.Name = "btnAreaCap";
+            this.btnAreaCap.Size = new System.Drawing.Size(210, 58);
+            this.btnAreaCap.TabIndex = 1;
+            this.btnAreaCap.Text = "Capture Area";
+            this.btnAreaCap.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(234, 136);
+            this.Controls.Add(this.btnAreaCap);
+            this.Controls.Add(this.btnScreenCap);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GyabzSS";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnScreenCap;
+        private System.Windows.Forms.Button btnAreaCap;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- GyabzSS/Form1.cs
+++ GyabzSS/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Imaging;
 
 namespace GyabzSS
 {
     public partial class Form1 : Form
     {
+        Bitmap bmp;
+        string saveTo;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnScreenCap_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Width); //sets bitmap to size of screen
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                saveTo = saveFileDialog1.FileName; //stores save location for screenshot
+                this.Hide();
+                Thread.Sleep(500); //hides screenshot box to get capture of screen
+                //take screenshot here
+
+                this.Show(); //shows screenshot box again
+            }
         }
     }
 }
